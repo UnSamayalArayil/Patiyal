53352bf22ca12eb76f8940ab4616d4add356d369
dnivra26 <dnivra26@gmail.com>
2015-05-09 11:44:20 +0530

    Arvind: register device with GCM and store reg id in shared preferences

--- app/src/main/java/io/github/dnivra26/unsamayalarayil/RegisterDeviceFragment.java
+++ app/src/main/java/io/github/dnivra26/unsamayalarayil/RegisterDeviceFragment.java
@@ -1,15 +1,40 @@
 package io.github.dnivra26.unsamayalarayil;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import java.io.IOException;
 
 
 public class RegisterDeviceFragment extends Fragment {
+
+    String SENDER_ID = "419810287584";
+    GoogleCloudMessaging gcm;
+    public static final String PROPERTY_REG_ID = "registration_id";
+    private static final String PROPERTY_APP_VERSION = "appVersion";
+
+
+    private final static int PLAY_SERVICES_RESOLUTION_REQUEST = 9000;
+    private static final String TAG = RegisterDeviceFragment.class.getSimpleName();
+    Button registerButton;
+    String regid;
 
     public static RegisterDeviceFragment newInstance() {
         RegisterDeviceFragment fragment = new RegisterDeviceFragment();
@@ -28,9 +53,88 @@ public class RegisterDeviceFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_register_device, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_register_device, container, false);
+        registerButton = (Button) rootView.findViewById(R.id.registerButton);
+        return rootView;
     }
 
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (checkPlayServices()) {
+            Toast.makeText(getActivity(), "Google play services available",Toast.LENGTH_SHORT).show();
+            gcm = GoogleCloudMessaging.getInstance(getActivity());
+            registerButton.setEnabled(true);
+        }
+
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                regid = getRegistrationId(getActivity());
+                if (regid.isEmpty()) {
+                    registerInBackground();
+                }
+            }
+        });
+    }
+
+    private void registerInBackground() {
+        new AsyncTask<Void,String,String>(){
+
+
+            @Override
+            protected String doInBackground(Void... voids) {
+                String msg = "";
+                try {
+                    if (gcm == null) {
+                        gcm = GoogleCloudMessaging.getInstance(getActivity());
+                    }
+                    regid = gcm.register(SENDER_ID);
+                    msg = "Device registered, registration ID=" + regid;
+                    sendRegistrationIdToBackend(regid);
+
+                    storeRegistrationId(getActivity(), regid);
+                } catch (IOException ex) {
+                    msg = "Error :" + ex.getMessage();
+                }
+                return msg;
+            }
+
+            @Override
+            protected void onPostExecute(String msg) {
+                Toast.makeText(getActivity(), "Device successfully registered: "+msg,Toast.LENGTH_SHORT).show();
+            }
+        }.execute();
+    }
+
+    private void storeRegistrationId(Context context, String regId) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        int appVersion = getAppVersion(context);
+        Log.i(TAG, "Saving regId on app version " + appVersion);
+        SharedPreferences.Editor editor = prefs.edit();
+        editor.putString(PROPERTY_REG_ID, regId);
+        editor.putInt(PROPERTY_APP_VERSION, appVersion);
+        editor.commit();
+    }
+
+    private void sendRegistrationIdToBackend(String regid) {
+
+    }
+
+    private boolean checkPlayServices() {
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(getActivity());
+        if (resultCode != ConnectionResult.SUCCESS) {
+            if (GooglePlayServicesUtil.isUserRecoverableError(resultCode)) {
+                GooglePlayServicesUtil.getErrorDialog(resultCode, getActivity(),
+                        PLAY_SERVICES_RESOLUTION_REQUEST).show();
+            } else {
+                Log.i(TAG, "This device is not supported.");
+                Toast.makeText(getActivity(), "Your device is not supported",Toast.LENGTH_SHORT).show();
+            }
+            return false;
+        }
+        return true;
+    }
 
     @Override
     public void onAttach(Activity activity) {
@@ -41,4 +145,42 @@ public class RegisterDeviceFragment extends Fragment {
     public void onDetach() {
         super.onDetach();
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        checkPlayServices();
+    }
+
+    private String getRegistrationId(Context context) {
+        final SharedPreferences prefs = getGCMPreferences(context);
+        String registrationId = prefs.getString(PROPERTY_REG_ID, "");
+        if (registrationId.isEmpty()) {
+            Log.i(TAG, "Registration not found.");
+            return "";
+        }
+        int registeredVersion = prefs.getInt(PROPERTY_APP_VERSION, Integer.MIN_VALUE);
+        int currentVersion = getAppVersion(context);
+        if (registeredVersion != currentVersion) {
+            Log.i(TAG, "App version changed.");
+            return "";
+        }
+        return registrationId;
+    }
+
+    private SharedPreferences getGCMPreferences(Context context) {
+        return getActivity().getSharedPreferences(RegisterDeviceFragment.class.getSimpleName(),
+                Context.MODE_PRIVATE);
+    }
+
+    private static int getAppVersion(Context context) {
+        try {
+            PackageInfo packageInfo = context.getPackageManager()
+                    .getPackageInfo(context.getPackageName(), 0);
+            return packageInfo.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            throw new RuntimeException("Could not get package name: " + e);
+        }
+    }
+
 }
