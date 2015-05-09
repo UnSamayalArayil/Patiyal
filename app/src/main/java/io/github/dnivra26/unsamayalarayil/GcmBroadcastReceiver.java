b58aa3dbd9810ad2293b7d947d4264c51513cefb
dnivra26 <dnivra26@gmail.com>
2015-05-09 12:18:13 +0530

    Arvind: Receive GCM message and show notification

--- app/src/main/java/io/github/dnivra26/unsamayalarayil/GcmBroadcastReceiver.java
+++ app/src/main/java/io/github/dnivra26/unsamayalarayil/GcmBroadcastReceiver.java
@@ -1,5 +1,7 @@
 package io.github.dnivra26.unsamayalarayil;
 
+import android.app.Activity;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.support.v4.content.WakefulBroadcastReceiver;
@@ -7,6 +9,9 @@ import android.support.v4.content.WakefulBroadcastReceiver;
 public class GcmBroadcastReceiver extends WakefulBroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-
+        ComponentName comp = new ComponentName(context.getPackageName(),
+                GcmIntentService.class.getName());
+        startWakefulService(context, (intent.setComponent(comp)));
+        setResultCode(Activity.RESULT_OK);
     }
 }
--- app/src/main/java/io/github/dnivra26/unsamayalarayil/GcmIntentService.java
+++ app/src/main/java/io/github/dnivra26/unsamayalarayil/GcmIntentService.java
@@ -1,10 +1,27 @@
 package io.github.dnivra26.unsamayalarayil;
 
 import android.app.IntentService;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.os.SystemClock;
+import android.support.v4.app.NotificationCompat;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 public class GcmIntentService extends IntentService {
 
+    public static final int NOTIFICATION_ID = 1;
+    private static final String TAG = GcmIntentService.class.getSimpleName();
+    private NotificationManager mNotificationManager;
+    NotificationCompat.Builder builder;
+
+    public GcmIntentService() {
+        super("GcmIntentService");
+    }
 
     public GcmIntentService(String name) {
         super(name);
@@ -12,6 +29,53 @@ public class GcmIntentService extends IntentService {
 
     @Override
     protected void onHandleIntent(Intent intent) {
+        Bundle extras = intent.getExtras();
+        GoogleCloudMessaging gcm = GoogleCloudMessaging.getInstance(this);
+        String messageType = gcm.getMessageType(intent);
+        if (GoogleCloudMessaging.
+                MESSAGE_TYPE_SEND_ERROR.equals(messageType)) {
+            sendNotification("Send error: " + extras.toString());
+        } else if (GoogleCloudMessaging.
+                MESSAGE_TYPE_DELETED.equals(messageType)) {
+            sendNotification("Deleted messages on server: " +
+                    extras.toString());
+        } else if (GoogleCloudMessaging.
+                MESSAGE_TYPE_MESSAGE.equals(messageType)) {
+            for (int i=0; i<5; i++) {
+                Log.i(TAG, "Working... " + (i + 1)
+                        + "/5 @ " + SystemClock.elapsedRealtime());
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                }
+            }
+            Log.i(TAG, "Completed work @ " + SystemClock.elapsedRealtime());
+            sendNotification("Received: " + extras.toString());
+            Log.i(TAG, "Received: " + extras.toString());
 
+        }
+
+        GcmBroadcastReceiver.completeWakefulIntent(intent);
     }
+
+    private void sendNotification(String msg) {
+        mNotificationManager = (NotificationManager)
+                this.getSystemService(Context.NOTIFICATION_SERVICE);
+
+        PendingIntent contentIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, MainActivity.class), 0);
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.drawable.ic_drawer)
+                        .setContentTitle("GCM Notification")
+                        .setStyle(new NotificationCompat.BigTextStyle()
+                                .bigText(msg))
+                        .setContentText(msg);
+
+        mBuilder.setContentIntent(contentIntent);
+        mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
+    }
+
+
 }
